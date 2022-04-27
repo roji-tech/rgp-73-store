e29833df10267bea93de41884da441bdef9fb99f
Roji-Tech <abdullahjamiu4@gmail.com>
2022-04-27 22:53:05 +0100

    index added

--- rgp-73-store/src/App.js
+++ rgp-73-store/src/App.js
@@ -19,7 +19,7 @@ function App() {
       <Navbar />
 
       <Routes>
-        <Route path='/' element={<Home />} />
+        <Route index path='/' element={<Home />} />
         <Route path='about' element={<About />} />
         <Route path='products' element={<Products />} >
           
