6a8a6c66bed6636dddba24ab86481ee102980d8a
Babatunde-Ben <babatundeben110.com>
2022-04-28 03:58:11 +0100

    modified banner

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -2,11 +2,12 @@ import React from "react";
 import "../css/banner.css";
 
 const Banner = () => {
-	return (
-		<div id="banner-wrap">
-			<div id="banner"></div>
-		</div>
-	);
+  return (
+    <div className="banner">
+      <h1 className="tagline">Eat healthy, live better</h1>
+      <button className="explore-btn">Explore &#8594;</button>
+    </div>
+  );
 };
 
 export default Banner;
