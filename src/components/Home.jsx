260570b51ebbc0f912f1413e6802e8331355191f
Babatunde-Ben <babatundeben110.com>
2022-04-28 05:07:18 +0100

    added value and testimonialls section

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,21 +2,67 @@ import React from "react";
 // Component import
 import Banner from "./Banner";
 import Products from "./Products";
+import value from "../img/meal-1.jpg";
 
 import "../css/home.css";
 
 const Home = () => {
-	return (
-		<>
-			<Banner />
-			<div>
-				<h2 style={{ fontSize: "60px" }}>RGP-73 Store</h2>
-			</div>
-			<div>
+  return (
+    <>
+      <Banner />
+      <section className="value">
+        <div>
+          <h2 className="title">
+            Why Tasty? <span></span>
+          </h2>
+          <p className="value-text">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum amet
+            accusamus voluptatibus cum rerum ipsam ad optio laudantium aut.
+            Iste, ab consequuntur veniam reprehenderit tempore enim! Saepe
+            placeat ea distinctio velit reprehenderit, cum eum officia
+            reiciendis nemo obcaecati sint nesciunt. Lorem ipsum dolor, sit amet
+            consectetur adipisicing elit. Repellat, explicabo
+          </p>
+        </div>
+        <div>
+          <img src={value} alt="" />
+        </div>
+      </section>
+
+      <section className="testimonials">
+        <h2 className="title">
+          Testimonials <span></span>
+        </h2>
+        <div className="testimonies">
+          <div className="testimony">
+            <p>
+              "Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+              Voluptates, ex!"
+            </p>
+            <h3 className="author">Prisca David</h3>
+          </div>
+          <div className="testimony">
+            <p>
+              "Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+              Voluptates, ex!"
+            </p>
+            <h3 className="author">Phillip Thomas</h3>
+          </div>
+          <div className="testimony">
+            <p>
+              "Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+              Voluptates, ex!"
+            </p>
+            <h3 className="author">Becca Willams</h3>
+          </div>
+        </div>
+      </section>
+
+      {/* <div>
 				<Products />
-			</div>
-		</>
-	);
+			</div> */}
+    </>
+  );
 };
 
 export default Home;
