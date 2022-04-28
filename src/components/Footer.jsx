ebce93285c227686b214849c500a2cd42956e103
Roji-Tech <abdullahjamiu4@gmail.com>
2022-04-28 21:33:17 +0100

    Merge branch 'main'

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,35 @@ import "../css/footer.css";
 const Footer = () => {
   return (
     <footer className="footer">
-      <h1> copyright &copy; SH React group 73</h1>
+      <div className="footer-row">
+        <div className="nav-logo">Tasty</div>
+        <div>
+          <p>
+            Purpose is to sustainably make Lorem ipsum dolor sit amet,
+            consectetur adipisicing elit. Voluptas reprehenderit doloribus
+            placeat est tenetur, deserunt ducimus quis labore saepe tempora?
+          </p>
+        </div>
+        <div>
+          <h3 className="footer-subtitle ">services</h3>
+          <ul>
+            <li>home deivery</li>
+            <li>quality food </li>
+            <li>affordable price </li>
+          </ul>
+        </div>
+        <div>
+          <h3 className="footer-subtitle">quick links</h3>
+          <ul>
+            <li>home</li>
+            <li>about </li>
+            <li>product </li>
+            <li>contact </li>
+          </ul>
+        </div>
+      </div>
+      <hr />
+      <p className="copyright"> copyright &copy; SideHustle React group 73</p>
     </footer>
   );
 };
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -34,7 +34,6 @@ const Home = () => {
 					<img src={value} alt="" />
 				</div>
 			</section>
-
 			<section className="testimonials">
 				<h2 className="custom-title">
 					Testimonials <span></span>
@@ -63,7 +62,34 @@ const Home = () => {
 					</div>
 				</div>
 			</section>
-
+			<section className="testimonials">
+				<h2 className="custom-title">
+					Testimonials <span></span>
+				</h2>
+				<div className="testimonies">
+					<div className="testimony">
+						<p>
+							"Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+							Voluptates, ex!"
+						</p>
+						<h3 className="author">Prisca David</h3>
+					</div>
+					<div className="testimony">
+						<p>
+							"Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+							Voluptates, ex!"
+						</p>
+						<h3 className="author">Phillip Thomas</h3>
+					</div>
+					<div className="testimony">
+						<p>
+							"Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+							Voluptates, ex!"
+						</p>
+						<h3 className="author">Becca Willams</h3>
+					</div>
+				</div>
+			</section>
 			<div>
 				<Products />
 			</div>
