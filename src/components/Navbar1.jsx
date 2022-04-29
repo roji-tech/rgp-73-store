aa7d03e0a0141ca6bfbe1ed0f86695517c1d5575
yusufom <yusufolamilekanmuktar@gmail.com>
2022-04-29 10:47:36 +0100

    Changed Click function

--- src/components/Navbar1.jsx
+++ src/components/Navbar1.jsx
@@ -5,28 +5,32 @@ import "../css/navbar1.css";
 
 const Navbar = () => {
 	const [isOpen, setIsOpen] = useState(false);
+
+	const openCloseNav = () => {
+		setIsOpen(!isOpen)
+	}
 	return (
-		<div className="Navbar">
+		<div className="Navbar" onClick={openCloseNav}>
 			<Link to={"/rgp-73-store"}>
 				<span className="nav-logo">Tasty</span>
 			</Link>
 			<div className={`nav-items ${isOpen && "open"}`}>
-				<Link to="/rgp-73-store" className="link" onClick={() => setIsOpen(!isOpen)}>
+				<Link to="/rgp-73-store" className="link" onClick={openCloseNav}>
 					HOME
 				</Link>
-				<Link to="/rgp-73-store/about" className="link" onClick={() => setIsOpen(!isOpen)}>
+				<Link to="/rgp-73-store/about" className="link" onClick={openCloseNav}>
 					ABOUT
 				</Link>
-				<Link to="/rgp-73-store/products" className="link" onClick={() => setIsOpen(!isOpen)}>
+				<Link to="/rgp-73-store/products" className="link" onClick={openCloseNav}>
 					PRODUCTS
 				</Link>
-				<Link to="/rgp-73-store/contact" className="link" onClick={() => setIsOpen(!isOpen)}>
+				<Link to="/rgp-73-store/contact" className="link" onClick={openCloseNav}>
 					CONTACT
 				</Link>
 			</div>
 			<div
 				className={`nav-toggle ${isOpen && "open"}`}
-				onClick={() => setIsOpen(!isOpen)}
+				onClick={openCloseNav}
 			>
 				<div className="bar"></div>
 			</div>
