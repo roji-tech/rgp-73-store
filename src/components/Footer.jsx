33e6ba468178b665b4813e7394fb43bb61ac0070
Babatunde-Ben <babatundeben110.com>
2022-05-03 02:35:21 +0100

    modified stylings

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,48 +4,55 @@ import { Link } from "react-router-dom";
 import "../css/footer.css";
 
 const Footer = () => {
-	return (
-		<footer className="footer">
-			<div className="footer-row">
-				<div className="nav-logo">Tasty</div>
-				<div>
-					<p>
-						Purpose is to sustainably make Lorem ipsum dolor sit amet,
-						consectetur adipisicing elit. Voluptas reprehenderit doloribus
-						placeat est tenetur, deserunt ducimus quis labore saepe
-						tempora?
-					</p>
-				</div>
-				<div>
-					<h3 className="footer-subtitle ">services</h3>
-					<ul>
-						<li>home deivery</li>
-						<li>quality food </li>
-						<li>affordable price </li>
-					</ul>
-				</div>
-				<div>
-					<h3 className="footer-subtitle">quick links</h3>
-					<ul>
-						<li>
-							<Link to={"/"} className="quick-links" >home </Link>
-						</li>
-						<li>
-							<Link to={"/about"} className="quick-links" >about </Link>
-						</li>
-						<li>
-							<Link to={"/products"} className="quick-links" >products </Link>
-						</li>
-						<li>
-							<Link to={"/contact"} className="quick-links" >contact </Link>
-						</li>
-					</ul>
-				</div>
-			</div>
-			<hr />
-			<p className="copyright">copyright &copy; SideHustle React group 73</p>
-		</footer>
-	);
+  return (
+    <footer className="footer">
+      <div className="footer-row">
+        <div className="nav-logo">Tasty</div>
+        <div>
+          <p>
+            Purpose is to sustainably make Lorem ipsum dolor sit amet,
+            consectetur adipisicing elit. Voluptas reprehenderit doloribus
+            placeat est tenetur, deserunt ducimus quis labore saepe tempora?
+          </p>
+        </div>
+        <div>
+          <h3 className="footer-subtitle ">services</h3>
+          <ul>
+            <li>home deivery</li>
+            <li>quality food </li>
+            <li>affordable price </li>
+          </ul>
+        </div>
+        <div>
+          <h3 className="footer-subtitle">quick links</h3>
+          <ul>
+            <li>
+              <Link to={"/"} className="quick-links">
+                home{" "}
+              </Link>
+            </li>
+            <li>
+              <Link to={"/about"} className="quick-links">
+                about{" "}
+              </Link>
+            </li>
+            <li>
+              <Link to={"/products"} className="quick-links">
+                products{" "}
+              </Link>
+            </li>
+            <li>
+              <Link to={"/contact"} className="quick-links">
+                contact{" "}
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <hr />
+      <p className="copyright">copyright &copy; SideHustle React Group 73</p>
+    </footer>
+  );
 };
 
 export default Footer;
